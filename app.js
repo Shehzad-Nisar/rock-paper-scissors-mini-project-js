e493220b8cc1b7984b68d19980aa3a26e19b7671
Shehzad-Nisar <shehzadnisar07@gmail.com>
2025-10-01 21:02:18 +0500

    Finally completed. Further improvement will be done later !!!

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let userScore = 0;
-let compscore = 0;
+let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+
+const msgPara = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#playerScore");
+const compScorePara = document.querySelector("#compScore");
 
 
 
@@ -16,62 +21,67 @@ const genCompchoice = () => {
 
 
 // draw match fuction:
-const drawfucn = () => {
-    console.log("match is draw.");
+const drawFucn = () => {
+    msgPara.innerText = 'Match is a draw!';
+    msgPara.style.backgroundColor = '#86A5D9'
+    msgPara.style.color = '#202A25'
+    
 
 
 }
 
+// winner or looser decision taking function as named showWinner();
 
-// computer win functions;
-const compWin = ()=> {
-    console.log("computer wins");
+const showWinner = (playerWin ,userChoice , compChoice) => {
+    if(playerWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msgPara.innerText = `You won!. Your ${userChoice} beat ${compChoice}.`;
+        msgPara.style.backgroundColor = '#26F0F1';
+        msgPara.style.color = '#202A25'
+
+    }else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msgPara.innerText = `You lost!. ${compChoice} beat your ${userChoice}.`;
+        msgPara.style.backgroundColor = '#5F4BB6';
+        msgPara.style.color = '#C4EBC8'
+    }
 }
 
-const playerWin = () => {
-    console.log("Player win");
-}
 // playGame() function will compare the user's choice and computer's choice to compare the choices and produces result on the basis of both player's choice .
 
 let playGame = (userChoice) => {
-    console.log("Player choice is " , userChoice);
-     
+    
     // generate computer's choice;
     const compChoice = genCompchoice();
-    console.log("Comp choice is ", compChoice);
+    
 
     // match draw:::
     if(userChoice === compChoice){
-        drawfucn();
+        drawFucn();
 
+    } else {
+      let playerWin = true;
+      if(userChoice === 'rock'){
+
+        // paper , scissors
+        playerWin = compChoice === 'paper' ? false : true;
+      } else if(userChoice === 'paper'){
+        // rock or scissors;
+
+        playerWin = compChoice === 'rock' ? true : false;
+
+      } else {
+        // rock or paper
+
+        playerWin = compChoice === 'rock' ? false : true;
+      }
+
+      showWinner(playerWin,userChoice, compChoice);
     }
 
-    //win situations:::
-    if(userChoice === 'rock' && compChoice === 'paper' ){
-        compWin();
 
-    }
-    if(userChoice === 'paper' && compChoice === 'rock' ){
-        playerWin();
-
-    }
-
-    if(userChoice === 'rock' && compChoice === 'scissors'){
-        playerWin();
-    }
-
-     if(userChoice === 'scissors' && compChoice === 'rock'){
-        compWin();
-    }
-
-
-    if(userChoice === 'paper' && compChoice === 'scissors'){
-        compWin();
-    }
-
-    if(userChoice === 'scissors' && compChoice === 'paper'){
-        playerWin();
-    }
 
     
 
